88cc8898564012f743659d1bad466e1508a44d4c
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-10-25 20:15:47 -0600

    game of life

---
+++ gol/src/Grid.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { gridDisplay, cellDisplay } from "./display";
+
+export default function Grid({ grid, toggleLife, clickable, gridSize }) {
+  return (
+    <div className="grid" style={gridDisplay(gridSize)}>
+      {/* inline styling allows for dynamic rendering of the grid and the cells */}
+      {grid.map((cell, i) => {
+        return (
+          <div
+            key={cell.id}
+            className={cell.alive ? "alive" : "dead"}
+            onClick={clickable ? toggleLife : null}
+            data-id={cell.id}
+            style={cellDisplay(cell.alive, gridSize)}
+          />
+        );
+      })}
+    </div>
+  );
+}
